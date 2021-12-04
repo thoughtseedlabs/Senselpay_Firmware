7c902e328acda6494017131c38560927b47c12bd
TrinetraM <72867195+TrinetraM@users.noreply.github.com>
2021-12-04 18:56:06 +0530

    AP name changed added averging

--- main.cpp
+++ main.cpp
@@ -7,14 +7,7 @@
 #include <NTPClient.h>
 #include <ESP8266HTTPClient.h>
 #include <ESP8266httpUpdate.h>
-#include <WiFiClientSecure.h>
-#include <CertStoreBearSSL.h>
-
-
-
-
 #define SIZE 512
-#define RSIZE 128
 using namespace std;
 
 typedef struct{
@@ -22,57 +15,65 @@ double powerVLF=0,powerLF=0,powerHF=0,BPM=0,coherence=0,phasic=0,tonic=0;
 } Data_t;
 
 /*variable dec*/
-IPAddress IP;
-int OSCPort;
+IPAddress IP(172,20,10,2);
+int OSCPort=8000;
 WiFiUDP timeUDP;
 NTPClient timeClient(timeUDP,"pool.ntp.org",19800);
-BearSSL::CertStore certStore;
+
 
 // DigiCert High Assurance EV Root CA
 
 
 /*Func declaration*/
 void getDatafromSensor(double rea[],double imag[],double &BPM,uint16_t size);
-void getGSRData(double rea[],double imag[],uint16_t size);
+double getGSRData(bool getPhaic ,bool getValue);
 void askCredentials(IPAddress &IPADDR,int &P);
 void FFT(double rea[],double imag[],unsigned int size,unsigned short int dir);
 void Magnitude(double rea[],double imag[],unsigned int size);
 void powerSpectrum(double mag[],double res[],unsigned int size);
 void OSC(Data_t data,IPAddress &IPADDR,int &P,NTPClient &client);
 double average(double arr[],uint16_t size);
+double average(vector<uint16_t> &vec);
 
-
+ //ADC_MODE(VDD33);
 void setup()
 {
  Serial.begin(115200);
  WiFi.hostname("SensePlay");
  WiFi.setOutputPower(20.5);
  askCredentials(IP,OSCPort);
+/* WiFi.begin("iPhone","mohan1711");
+ while(WiFi.status()!=WL_CONNECTED)
+ {
+   delay(300);
+ }*/
  timeClient.begin();
 }
 void loop()
 {
   static uint32_t previousFFTTime=0;
-  static uint32_t GSRTime=0;
+  static uint32_t phasicTime=millis(),tonicTime=millis();
   uint32_t entryTime;
   static Data_t data;
   static double BPM=0;
   static double real[SIZE];
   static double imaginary[SIZE];
   static double power[SIZE];
-  static double resisReal[RSIZE];
-  static double resisImaginary[RSIZE];
-  static double resisPower[RSIZE];
   double totalpower=0;
   if(WiFi.status()!=WL_CONNECTED)
   {
     ESP.reset();
   }
   getDatafromSensor(real,imaginary,BPM,SIZE);
-  if(millis()-GSRTime>=1000)
+  if(millis()-phasicTime>=250)
   {
-    getGSRData(resisReal,resisImaginary,RSIZE);
-    GSRTime=millis();
+    getGSRData(true,false);
+    phasicTime=millis();
+  }
+  if(millis()-tonicTime>=1000)
+  {
+    getGSRData(false,false);
+    tonicTime=millis();
   }
   if(millis()-previousFFTTime>=2000)
   {
@@ -80,9 +81,6 @@ void loop()
     FFT(real,imaginary,SIZE,FFT_FORWARD);
     Magnitude(real,imaginary,SIZE);
     powerSpectrum(real,power,SIZE);
-    FFT(resisReal,resisImaginary,RSIZE,FFT_FORWARD);
-    Magnitude(resisReal,resisImaginary,RSIZE);
-    powerSpectrum(resisReal,resisPower,RSIZE);
     data.BPM=0;
     data.coherence=0;
     data.phasic=0;
@@ -113,15 +111,10 @@ void loop()
     }
     Serial.printf("data power hf %f\n",data.powerHF);
     data.powerHF/=totalpower;
-    for(int i=0; i<4; i++)
-    {
-      data.tonic+=resisPower[i];
-    }
-    
-    for(int i=4;i<RSIZE; i++)
-    {
-      data.phasic+=resisPower[i];
-    }
+    data.phasic=getGSRData(true,true);
+    data.tonic=getGSRData(false,true);
+    Serial.printf("average value of phasic %f\n",data.phasic);
+    Serial.printf("Average value of tonic %f\n",data.tonic);
     data.coherence=data.powerLF/(data.powerVLF+data.powerHF);
     data.BPM=BPM;
     Serial.println(BPM);
@@ -212,46 +205,49 @@ void getDatafromSensor(double rea[],double imag[],double &BPM,uint16_t size)
    
 }
 
-void getGSRData(double rea[],double imag[],uint16_t size)
+double getGSRData(bool getPhasic=false,bool getValue=false)
 {
  static bool init=false;
- static vector<uint16_t> resistVec;
+ static vector<uint16_t> phasic;
+ static vector<uint16_t> tonic;
  uint16_t sample;
- double resistance=(1024+2*sample)*10000/(512-sample);
- if(!init)
+  if(!init)
  {
-  // pinMode(PIN_A0,INPUT);
-   resistVec.reserve(size+2);
+   phasic.reserve(64);
+   tonic.reserve(256);
    init=true;
  }
- for(uint16_t i=0; i<size; i++)
+ if(!getValue)
  {
-   rea[i]=0;
-   imag[i]=0;
- }
  sample=analogRead(A0);
- 
- Serial.printf("sample %d\n",sample);
- //Serial.printf("resistance =%f\n",resistance);
- if(sample>50)
+ if(getPhasic)
  {
-   resistance=(1024+2*sample)*10000;
-   resistance=resistance/(512-sample);
-   resistVec.push_back(resistance);
-   for(uint16_t i=0; i<resistVec.size();i++)
+   phasic.push_back(sample);
+   if(phasic.size()>64)
    {
-     rea[i]=(1024+2*resistVec[i])*10000/(SIZE-resistVec[i]);
+     phasic.erase(phasic.begin());
    }
  }
  else
  {
-   resistVec.clear();
-   for(uint16_t i=0; i<size; i++)
+   tonic.push_back(sample);
+   if(tonic.size()>256)
    {
-     rea[i]=INFINITY;
+     tonic.erase(tonic.begin());
    }
  }
- 
+ return NAN;
+ }
+ else
+ {
+   if(getPhasic)
+   {
+     return average(phasic);
+   }
+   
+       return average(tonic);
+  
+ }
 } 
 
 void askCredentials(IPAddress &IPADDR,int &P) 
@@ -266,7 +262,7 @@ void askCredentials(IPAddress &IPADDR,int &P)
     WiFiManagerParameter OSCIP("OSC IP","OSC Port","198.162.4.1",20);
     wifiManager.addParameter(&OSCIP);
     wifiManager.addParameter(&OSCPORT);
-    wifiManager.startConfigPortal("SensePlay");
+    wifiManager.startConfigPortal("TOM Flow Compass");
     wifiManager.setConfigPortalTimeout(300);
     String ip = WiFi.localIP().toString();
     String systemIpInfo = "IP: " + ip + " Hostname: ";
@@ -336,5 +332,14 @@ double average(double array[],uint16_t size)
     average+=array[i];
   }
  return average/size;
+}
+double average(vector<uint16_t> &vec)
+{
+  double avg=0;
+  for(auto itr=vec.begin();itr!=vec.end();++itr)
+  {
+    avg+=*itr;
+  }
+  return avg/vec.size();
 }
 
